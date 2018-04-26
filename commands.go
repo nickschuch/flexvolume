f803308bf2366b8002111071ead482ab62e96cd0
Nick Schuch <nick@myschuch.com>
2018-04-26 21:48:54 +1000

    Update to K8s 1.9

--- commands.go
+++ commands.go
@@ -17,6 +17,19 @@ func Commands(fv FlexVolume) []cli.Command {
 			},
 		},
 		{
+			Name:  "getvolumename",
+			Usage: "Returns the unique name of the volume",
+			Action: func(c *cli.Context) error {
+				var opts map[string]string
+
+				if err := json.Unmarshal([]byte(c.Args().Get(1)), &opts); err != nil {
+					return err
+				}
+
+				return handle(fv.GetVolumeName(opts))
+			},
+		},
+		{
 			Name:  "attach",
 			Usage: "Attach the volume",
 			Action: func(c *cli.Context) error {
@@ -30,10 +43,56 @@ func Commands(fv FlexVolume) []cli.Command {
 			},
 		},
 		{
+			Name:  "waitforattach",
+			Usage: "Waits until a volume is fully attached to a node and its device emerges",
+			Action: func(c *cli.Context) error {
+				var opts map[string]string
+
+				if err := json.Unmarshal([]byte(c.Args().Get(2)), &opts); err != nil {
+					return err
+				}
+
+				return handle(fv.WaitForAttach(c.Args().Get(1), opts))
+			},
+		},
+		{
 			Name:  "detach",
 			Usage: "Detach the volume",
 			Action: func(c *cli.Context) error {
-				return handle(fv.Detach(c.Args().Get(1)))
+				return handle(fv.Detach(c.Args().Get(1), c.Args().Get(2)))
+			},
+		},
+		{
+			Name:  "isattached",
+			Usage: "Checks that a volume is attached to a node",
+			Action: func(c *cli.Context) error {
+				var opts map[string]string
+
+				if err := json.Unmarshal([]byte(c.Args().Get(1)), &opts); err != nil {
+					return err
+				}
+
+				return handle(fv.Detach(opts, c.Args().Get(2)))
+			},
+		},
+		{
+			Name:  "mountdevice",
+			Usage: "Mounts a volume’s device to a directory",
+			Action: func(c *cli.Context) error {
+				var opts map[string]string
+
+				if err := json.Unmarshal([]byte(c.Args().Get(3)), &opts); err != nil {
+					return err
+				}
+
+				return handle(fv.MountDevice(c.Args().Get(1), c.Args().Get(2), opts))
+			},
+		},
+		{
+			Name:  "unmountdevice",
+			Usage: "Unmounts a volume’s device from a directory",
+			Action: func(c *cli.Context) error {
+				return handle(fv.UnmountDevice(c.Args().Get(1)))
 			},
 		},
 		{
--- types.go
+++ types.go
@@ -3,21 +3,28 @@ package flexvolume
 type Status string
 
 const (
-	StatusSuccess Status       = "Success"
-	StatusFailure Status       = "Failure"
-	StatusNotSupported Status  = "Not Supported"
+	StatusSuccess      Status = "Success"
+	StatusFailure      Status = "Failure"
+	StatusNotSupported Status = "Not Supported"
 )
 
 type FlexVolume interface {
 	Init() Response
+	GetVolumeName(map[string]string) Response
 	Attach(map[string]string) Response
-	Detach(string) Response
-	Mount(string, string, map[string]string) Response
+	WaitForAttach(string, map[string]string) Response
+	IsAttached(map[string]string, string) Response
+	Detach(string, string) Response
+	MountDevice(string, string, map[string]string) Response
+	UnmountDevice(string) Response
+	Mount(string, map[string]string) Response
 	Unmount(string) Response
 }
 
 type Response struct {
-	Status  Status `json:"status"`
-	Message string `json:"message"`
-	Device  string `json:"device"`
+	Status     Status `json:"status"`
+	Message    string `json:"message"`
+	Device     string `json:"device"`
+	VolumeName string `json:"volumeName"`
+	Attached   bool   `json:"attached"`
 }
